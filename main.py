54153a371999df7b259cb9808f94c35b3e6b3bff
George <62718935+GeorgePavlov1580@users.noreply.github.com>
2022-02-07 20:35:00 +0300

    Update main.py
    Small code refactoring and addition of "Processing language" and "C" languages

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ def main():
     # Welcome message
     print(
         "Hello, this program will simplify\nthe process of creating a lot of files with the same name!\nPress any "
-        "key to continue...") 
+        "key to continue...")
     input()
 
     # Inputting the directory, quantity and name of files
     # === directory ===
     print("Input directory you wish to create files in:", end=' ')
-    directory = str(input())
+    fileDirectory = str(input())
 
     # === quantity ===
     print("Input how many files you want to create:", end=' ')
-    quantity = int(input())
-    quantity += 1
+    fileQuantity = int(input())
 
     # !=! check for validity !=!
-    if quantity == 0:
-        quantity = 1
-    elif quantity < 0:
-        quantity = abs(quantity)
+    if fileQuantity == 0:
+        fileQuantity = 1
+    elif fileQuantity < 0:
+        fileQuantity = abs(fileQuantity)
+
+    fileQuantity += 1
 
     # === name ===
     print("Input a name of your file:", end=' ')
-    name = str(input())
+    fileName = str(input())
 
     # === format ===
     print("Input the file's format (for example cpp):", end=' ')
@@ -31,23 +32,28 @@ def main():
 
     # === snippets ===
     print("Do you want to create \"Hello, world!\" snippet? (Y/N):", end=' ')
-    snippet = str(input()).lower()
+    snippetEnabled = str(input()).lower()
 
     # Creating files
-    for i in range(1, quantity):
-        file = open(f"{directory}/{name}{i}.{fileFormat}", "a")
+    for i in range(1, fileQuantity):
+        file = open(f"{fileDirectory}/{fileName}{i}.{fileFormat}", "a")
 
-        if (fileFormat == "cpp") and (snippet == 'y'):
+        if (fileFormat == "cpp") and (snippetEnabled == 'y'):
             file.write("#include \"iostream\"\n\nint main(){\n\n\treturn 0;\n}")
-        elif (fileFormat == "py") and (snippet == 'y'):
+        elif (fileFormat == "py") and (snippetEnabled == 'y'):
             file.write("def main():\n\treturn\n\nif __name__ == \'__main__\':\n\tmain()")
-        elif (fileFormat == "java") and (snippet == 'y'):
+        elif (fileFormat == "java") and (snippetEnabled == 'y'):
             file.write("class Main{\n    public static void main(String[] args){\n        System.out.println(\"Hello "
                        "World!\");\n    }\n}")
-        elif (fileFormat == "cs") and (snippet == 'y'):
+        elif (fileFormat == "cs") and (snippetEnabled == 'y'):
             file.write("namespace Main\n{\n    class Main{         \n        static void Main(string[] args)\n        "
                        "{\n            System.Console.WriteLine(\"Hello World!\");\n        }\n    }\n}")
-
+        elif (fileFormat == "pde") and (snippetEnabled == 'y'):
+            file.write("void setup() {\n  size(640, 360);\n  stroke(255);\n}\n\nvoid draw(){\n  background(0);\n  \n  "
+                       "fill(80);\n  noStroke();\n}")
+        elif (fileFormat == "c") and (snippetEnabled == 'y'):
+            file.write("#include <stdio.h>\n\nint main() {\n   printf(\"Hello, World!\");\n\n   return 0;\n}")
+            
         file.close()
 
     print("Done!\n")
